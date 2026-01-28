120b159bef8bd32e658e7caed77380e096c5c52c
samuel <esuolasamuel7@gmail.com>
2026-01-28 20:33:24 +0100

    service's height fix

--- components/section/Services.jsx
+++ components/section/Services.jsx
@@ -94,7 +94,7 @@ export default function Services() {
 
   return (
     <section id="services" className="bg-linear-to-br from-[#0b1020] to-[#05060d] text-white p-3.5 sm:p-7.5 md:p-15 lg:p-30">
-      <div className="grid md:grid-cols-2 gap-4 sm:gap-6 md:gap-8 lg:gap-10 xl:gap-12 2xl:gap-14 items-center h-209">
+      <div className="grid md:grid-cols-2 gap-4 sm:gap-6 md:gap-8 lg:gap-10 xl:gap-12 2xl:gap-14 items-center min-h-120 sm:min-h-140 md:min-h-160 lg:min-h-180 xl:min-h-200 2xl:min-h-220">
 
         {/* LEFT */}
         <div className=" max-w-2xl block lg:block">
